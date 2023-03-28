df6630f02a33d27a1610d50f0a7b5dff047e2165
ngotfryd123 <nick_gotfryd@hotmail.com>
2023-03-28 10:11:31 -0700

    Added basic math functions

---
+++ script.js
@@ -0,0 +1,16 @@
+function add(num1,num2){
+    let sum = num1+num2;
+    return sum;}
+
+function subtract(num1, num2){
+    let difference= num1-num2;
+    return difference;}
+
+function multiply(num1,num2){
+    let product= num1*num2;
+    return product;}
+
+function divide(num1,num2){
+    let quotient= num1/num2;
+    return quotient;}
+    
