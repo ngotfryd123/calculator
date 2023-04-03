182e48ca9905da0cad2d0d55ef1b515d87ef6287
ngotfryd123 <nick_gotfryd@hotmail.com>
2023-04-03 12:35:42 -0700

    Added functionality to calc

--- script.js
+++ script.js
@@ -1,41 +1,57 @@
-function add(num1,num2){
-    let sum = num1+num2;
-    return sum;}
-
-function subtract(num1, num2){
-    let difference= num1-num2;
-    return difference;}
-
-function multiply(num1,num2){
-    let product= num1*num2;
-    return product;}
-
-function divide(num1,num2){
-    let quotient= num1/num2;
-    return quotient;}
-
-function operate(num1,num2,operator){
-        operator='+' ? add(num1,num2) 
-        :operator='-' ? subtract(num1,num2)
-        :operator='*' ? multiply(num1,num2)
-        :divide(num1,num2);}
-
-function getDisplay(buttonVal){
-    if (buttonVal==="c"){
-        displayVar="";
-        buttonVal=displayVar;}
-    displayVar+=buttonVal;
-    return changeDisplay(displayVar);}
-
-function changeDisplay(setDisplay){
-    const display = document.querySelector('#display')
-    display.innerHTML = setDisplay;}
-
-const container = document.querySelector('#container');
-container.style.background = "dark blue";
-//listeners.addEventListener('click')
-
 let num1=0;
 let num2=0;
 let operator;
 let displayVar="";
+
+function opDisplay(value){
+const display = document.querySelector('#display');
+        display.innerHTML = value;}
+
+function add(a,b){
+    let sum = a+b;
+    return opDisplay(sum);}
+
+function subtract(a, b){
+    let difference= a-b;
+    return opDisplay(difference);}
+
+function multiply(a,b){
+    let product= a*b;
+    return opDisplay(product);}
+
+function divide(a,b){
+    let quotient= a/b;
+    return opDisplay(quotient);}
+
+function getDisplay(buttonVal){
+    if (buttonVal==="c"){
+        displayVar="";
+        buttonVal=displayVar;
+        operator=displayVar;}
+
+    if (buttonVal==="+" || buttonVal==="-" || buttonVal ==="/" || buttonVal ==="*"){
+        operator=buttonVal;
+        num1=parseInt(displayVar);}
+
+    if (buttonVal==="="){
+        if (displayVar.includes("-"))num2=parseInt(displayVar.split('-')[1]);
+        else if (displayVar.includes("+"))num2=parseInt(displayVar.split('+')[1]);
+        else if (displayVar.includes("*"))num2=parseInt(displayVar.split('*')[1]);
+        else num2=parseInt(displayVar.split('/')[1]);}
+
+    displayVar+=buttonVal;
+    return changeDisplay(displayVar);}
+
+function operate(a,b,c){
+        c==='+' ? add(a,b) 
+        :c==='-' ? subtract(a,b)
+        :c==='*' ? multiply(a,b)
+        :divide(a,b);}
+    
+function changeDisplay(setDisplay){
+        const display = document.querySelector('#display');
+        display.innerHTML = setDisplay;
+        if (setDisplay.includes("="))operate(num1,num2,operator);}   
+
+const container = document.querySelector('#container');
+container.style.background = "dark blue";
